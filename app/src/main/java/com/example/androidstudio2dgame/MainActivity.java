ba257423983a3abfa6f0036ea0d42ef25491edf2
hceun <whitestar718@naver.com>
2023-04-18 19:59:50 +0900

    Game loop

--- app/src/main/java/com/example/androidstudio2dgame/MainActivity.java
+++ app/src/main/java/com/example/androidstudio2dgame/MainActivity.java
@@ -3,12 +3,29 @@ package com.example.androidstudio2dgame;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.example.androidstudio2dgame.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
+    private Game game;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        // set window to fullscreen (will hide status bar)
+        Window window = getWindow();
+        window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+        );
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        game = new Game(this);
+        setContentView(game);
     }
+
 }
